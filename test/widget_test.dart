e49a0795b48896d74fb5580343cf530747e3566b
S-YUN <https://github.com/S-YUN>
2024-08-16 15:22:19 +0900

    chore: 파일 구조 정리

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:real_time_subway/app.dart';
+import 'package:real_time_subway/presentation/subway/subway_screen.dart';
 
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const SubwayScreen());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
