9ccc7b31dcebf88089d032751c10a1effbab7fb2
S-YUN <https://github.com/S-YUN>
2024-08-20 02:13:34 +0900

    fix: ui state에서 fromJson 삭제

--- lib/presentation/subway/subway_screen_ui_state.dart
+++ lib/presentation/subway/subway_screen_ui_state.dart
@@ -2,7 +2,6 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:real_time_subway/domain/model/subway_real_time_info.dart';
 
 part 'subway_screen_ui_state.freezed.dart';
-part 'subway_screen_ui_state.g.dart';
 
 @freezed
 class SubwayScreenUiState with _$SubwayScreenUiState {
@@ -11,6 +10,4 @@ class SubwayScreenUiState with _$SubwayScreenUiState {
     @Default([]) List<SubwayRealTimeInfo> subwayInfos,
   }) = _SubwayScreenUiState;
 
-  factory SubwayScreenUiState.fromJson(Map<String, Object?> json) =>
-      _$SubwayScreenUiStateFromJson(json);
 }
